fd84af99fe42145dcff0ff385256a9ff4207fd5d
Marilia Faria <mariliafs_@hotmail.com>
2024-09-25 18:42:34 -0300

    aumenta tamanho do banner no sm

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = (props) =>{
 
     const {href, text, extraClasses} = props;
-    const classes = `${extraClasses} text-white text-sm px-4 py-2 rounded-full hover:bg-orange-700 lg:text-lg lg:px-6 lg:py-3 lg:rounded-full`;
+    const classes = `${extraClasses}  text-white text-sm px-4 py-2 rounded-full hover:bg-orange-700 lg:text-lg lg:px-6 lg:py-3 lg:rounded-full`;
 
     return(
         <button href={href} className={classes} aria-label={text}>
