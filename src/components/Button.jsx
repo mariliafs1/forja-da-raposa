5904d9e2230680676bdf622f3ce12efa909c2ea3
Marilia Faria <mariliafs_@hotmail.com>
2024-09-26 12:23:52 -0300

    Cria card1

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,12 +3,11 @@ import React from 'react';
 const Button = (props) =>{
 
     const {href, text, extraClasses} = props;
-    const classes = `${extraClasses}  text-white text-sm px-4 py-2 rounded-full hover:bg-orange-700 lg:text-lg lg:px-6 lg:py-3 lg:rounded-full`;
-
+    const classes = `${extraClasses} text-white text-sm px-4 py-2 rounded-full mt-8 hover:bg-orange-700 lg:text-lg lg:px-6 lg:py-3 lg:rounded-full`;
     return(
-        <button href={href} className={classes} aria-label={text}>
+       <a href={href} target='_blank'><button href={href} className={classes} aria-label={text}>
             {text}
-        </button>
+        </button></a>
     )
 }
 
